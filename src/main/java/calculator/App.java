70be8e4ac30c3eced46d5fb32bd9ebce15c40962
Robbie <robbie331@>
2024-04-30 16:31:46 +0900

    Level 1-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -15,7 +15,10 @@ public class App {
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num2 = sc.nextInt();
 
-        System.out.println("num1 , num2= " + num1 +", "+ num2);
+        System.out.println("사칙연산 기호를 입력하세요 :");
+        // 스트링을 입력받되 맨 처음 인덱스의 char를 가져옴
+        char calChar = sc.next().charAt(0);
+        System.out.println("calChar = " + calChar);
 
     }
 }
